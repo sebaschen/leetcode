69969fcee2c7de0565e0e4e42abd2be5f43f7686
lipuchen <sphilip1206@gmail.com>
2020-04-27 15:46:46 +0800

    525 added

---
+++ 525_contiguous.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        '''
+        check if the current sum has the same before, if same sum index now - previous index.
+        To get the previous index, we save it as: table[count]=index
+    
+        '''
+        count = 0
+        max_length=0
+        table = {0: 0}
+        for index, num in enumerate(nums,1):#remember, index starts at 1
+            if num ==0:
+                count-=1
+            elif num == 1:
+                count+=1
+            if count in table:
+                max_length = max(max_length,index-table[count])
+            else:
+                table[count]=index
+        return max_length
