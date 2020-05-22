0021f761032e14de19218af91f0f73f0a11c6799
lipuchen <sphilip1206@gmail.com>
2020-05-22 10:19:15 +0800

    141 added

---
+++ 141_Linked_List_Cycle.py
@@ -0,0 +1,21 @@
+# 141. Linked List Cycle
+# 141_Linked_List_Cycle
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        """
+        if the slow one is euqal to the sql
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
