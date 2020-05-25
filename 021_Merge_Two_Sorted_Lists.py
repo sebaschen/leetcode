981280be6d188203d8bc784da367964780f0fea7
lipuchen <sphilip1206@gmail.com>
2020-05-25 16:03:10 +0800

    michelle solution updated

--- 021_Merge_Two_Sorted_Lists.py
+++ 021_Merge_Two_Sorted_Lists.py
@@ -8,29 +8,44 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         
-        result = []
-        dummy = ListNode(0) #use dummy as the pointer
-        p = dummy #use p as the initial pointer that can directly traverse through the linkedlist
+        curr = dummy = ListNode(0)
         
-        #1. always consider the boundary case
-        if not l1 and not l2:
-            return None
-        
-        #Main:        
-        """
-        1. Append 2 linkelist to one list
-        2. sort the list
-        3. put the list to a linkedlist
-        """
-        while l1 or l2:
-            if l1:
-                result.append(l1.val)
-                l1 =l1.next
-            if l2:
-                result.append(l2.val)
+        while l1 and l2:
+            if l1.val < l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
                 l2 = l2.next
-        result = sorted(result)
-        for i in result:
-            p.next = ListNode(int(i))
-            p = p.next
+            curr = curr.next
+        curr.next = l1 or l2
         return dummy.next
+            
+        
+        
+#         result = []
+#         dummy = ListNode(0) #use dummy as the pointer
+#         p = dummy #use p as the initial pointer that can directly traverse through the linkedlist
+        
+#         #1. always consider the boundary case
+#         if not l1 and not l2:
+#             return None
+        
+#         #Main:        
+#         """
+#         1. Append 2 linkelist to one list
+#         2. sort the list
+#         3. put the list to a linkedlist
+#         """
+#         while l1 or l2:
+#             if l1:
+#                 result.append(l1.val)
+#                 l1 =l1.next
+#             if l2:
+#                 result.append(l2.val)
+#                 l2 = l2.next
+#         result = sorted(result)
+#         for i in result:
+#             p.next = ListNode(int(i))
+#             p = p.next
+#         return dummy.next
